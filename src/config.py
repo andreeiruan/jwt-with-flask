9901e536d9fc53520f404dfe775c4d9dd39b36f7
andreeiruan <andreicruz.silva8765@gmail.com>
2020-09-06 13:08:57 -0300

    ajuste

--- src/config.py
+++ src/config.py
@@ -7,6 +7,6 @@ key = ''.join(random.choice(random_str) for i in range(12))
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'mysql://root:Gre1903@$@localhost:3306/jwtwithflask'
+SQLALCHEMY_DATABASE_URI = 'mysql://<user>:<senha>$@localhost:3306/jwtwithflask'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = key
